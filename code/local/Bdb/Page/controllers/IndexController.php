30e57b4ee4b56b5e660fa4cc89b5e0fd310e8a39
ldhlfzysys <ldhlfzysys@163.com>
2017-07-09 22:00:43 +0800

    创建bdbpage模块

---
+++ code/local/Bdb/Page/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+class Bdb_Page_IndexController extends Mage_Core_Controller_Front_Action {          
+    public function indexAction() {  
+        echo 'Hello World!';  
+    }  
+    public function goodbyeAction() {  
+    	echo 'Goodbye World!';  
+	}  
+ 
+} 
+
