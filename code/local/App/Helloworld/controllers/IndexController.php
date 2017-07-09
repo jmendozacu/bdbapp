d98eecc0ca430ee880111352e0e6c930fa3eb1d2
ldhlfzysys <ldhlfzysys@163.com>
2017-07-09 21:03:10 +0800

    还原configviewer

--- code/local/App/Helloworld/controllers/IndexController.php
+++ code/local/App/Helloworld/controllers/IndexController.php
@@ -4,6 +4,16 @@ class App_Helloworld_IndexController extends Mage_Core_Controller_Front_Action {
     public function indexAction() {  
         echo 'Hello World!';  
     }  
+    public function goodbyeAction() {  
+    	echo 'Goodbye World!';  
+	}  
+    public function paramsAction() {  
+	    echo '<dl>';              
+	    foreach($this->getRequest()->getParams() as $key=>$value) {  
+	        echo '<dt><strong>Param: </strong>'.$key.'</dt>';  
+	        echo '</dl><dl><strong>Value: </strong>'.$value.'</dl>';  
+	    }  
+	    echo '';  
+	}  
 } 
 
-?>
--- code/local/Vt/Configviewer/controllers/IndexController.php
+++
@@ -1,5 +0,0 @@
-class Vt_Configviewer_IndexController extends Mage_Core_Controller_Front_Action {          
-    public function indexAction() {  
-        echo 'Hello World!';  
-    }  
-}  
