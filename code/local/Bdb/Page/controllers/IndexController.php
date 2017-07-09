6b1f64341bd6069916873bf60e0501c6e2cf8191
ldhlfzysys <ldhlfzysys@163.com>
2017-07-09 22:19:21 +0800

    测试

--- code/local/Bdb/Page/controllers/IndexController.php
+++ code/local/Bdb/Page/controllers/IndexController.php
@@ -2,7 +2,8 @@
 
 class Bdb_Page_IndexController extends Mage_Core_Controller_Front_Action {          
     public function indexAction() {  
-        echo 'Hello World!';  
+        $this->loadLayout();  
+    	$this->renderLayout();  
     }  
     public function goodbyeAction() {  
     	echo 'Goodbye World!';  
