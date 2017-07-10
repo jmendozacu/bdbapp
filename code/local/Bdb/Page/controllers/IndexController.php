30b6081715bb229dcec1075b4815c78a296ba119
ldhlfzysys <ldhlfzysys@163.com>
2017-07-10 22:05:42 +0800

    test

--- code/local/Bdb/Page/controllers/IndexController.php
+++ code/local/Bdb/Page/controllers/IndexController.php
@@ -2,8 +2,9 @@
 
 class Bdb_Page_IndexController extends Mage_Core_Controller_Front_Action {          
     public function indexAction() {  
-        $this->loadLayout();  
-    	$this->renderLayout();  
+     //    $this->loadLayout();  
+    	// $this->renderLayout(); 
+    	echo 'asdf'; 
     }  
     public function goodbyeAction() {  
     	echo 'Goodbye World!';  
