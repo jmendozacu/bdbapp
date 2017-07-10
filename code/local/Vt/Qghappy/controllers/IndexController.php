221f0e57015ebee5c6904581cc065d4685edd35e
jingyu11 <jingyu11@staff.weibo.com.cn>
2017-07-10 23:37:12 +0800

    layout

--- code/local/Vt/Qghappy/controllers/IndexController.php
+++ code/local/Vt/Qghappy/controllers/IndexController.php
@@ -1,7 +1,8 @@
 <?php
 
 class Vt_Qghappy_IndexController extends Mage_Core_Controller_Front_Action {          
-    public function indexAction() {  
-        echo 'Hello World!';  
-    }  
+    public function indexAction() {    
+    	$this->loadLayout();  
+    	$this->renderLayout();  
+	} 
 }
