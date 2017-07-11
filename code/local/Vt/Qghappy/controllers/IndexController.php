b75bcdc0ae566e0d360e9ad50aa9f53932290178
jingyu11 <jingyu11@staff.weibo.com.cn>
2017-07-11 21:18:48 +0800

    位置调整

--- code/local/Vt/Qghappy/controllers/IndexController.php
+++ code/local/Vt/Qghappy/controllers/IndexController.php
@@ -1,9 +1,8 @@
 <?php
 
 class Vt_Qghappy_IndexController extends Mage_Core_Controller_Front_Action {          
-    public function indexAction() {
-    	echo 'Hello Index!';   
-    	// $this->loadLayout();  
-    	// $this->renderLayout();  
+    public function indexAction() {   
+    	$this->loadLayout();  
+    	$this->renderLayout();  
 	} 
 }
