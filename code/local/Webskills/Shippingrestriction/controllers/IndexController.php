5f9153e5210240c78058a24e4d3500b5bd91bc01
ldhlfzysys <ldhlfzysys@163.com>
2017-11-04 15:23:03 +0800

    用户自行检测邮编可用性

--- code/local/Webskills/Shippingrestriction/controllers/IndexController.php
+++ code/local/Webskills/Shippingrestriction/controllers/IndexController.php
@@ -30,14 +30,47 @@ class Webskills_Shippingrestriction_IndexController extends Mage_Core_Controller
 		}
 		$address = $_REQUEST['zipcode'];	
 
-		if (in_array($address,$data))
-		{
-			echo '<span style="color:#E84100">Oops!Shipping is not available for your location</span>';
+		if (!$this->_checkZipcode($address,$data)) {
+			echo '<span style="color:#E84100">很抱歉，您目前所在邮编还未开放。</span>';
 		}
 		else
 		{
-			echo '<span style="color:#5A9800">Congratulations!Shipping is Available for your location</span>';
-		}	
+			echo '<span style="color:#5A9800">恭喜你！您所在邮编可以送货上门！</span>';
+		}
+		// if (in_array($address,$data))
+		// {
+		// 	echo '<span style="color:#E84100">Oops!Shipping is not available for your location</span>';
+		// }
+		// else
+		// {
+		// 	echo '<span style="color:#5A9800">Congratulations!Shipping is Available for your location</span>';
+		// }	
 		
 	}
+
+	public function _checkZipcode($zipcode,$zipcodes)
+    {
+        foreach ($zipcodes as $value)
+        {
+            if (strstr($value,"*"))
+            {
+                $newvalue = str_replace("*", "", $value);
+                $zipcode_space = str_replace(" ", "", $zipcode);
+                $zipcode_up = strtoupper($zipcode_space);
+                if(strpos($zipcode_up,$newvalue) === 0)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                $zipcode_space = str_replace(" ", "", $zipcode);
+                $zipcode_up = strtoupper($zipcode_space);
+                if ($zipcode_up == $value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
