5046d82565346f72b5cdd617314cb374ab01118d
Dwight <ldhlfzysys@163.com>
2018-02-28 21:51:26 +0800

    处理因为session导致chrome无法登陆

--- code/core/Mage/Core/Model/Session/Abstract/Varien.php
+++ code/core/Mage/Core/Model/Session/Abstract/Varien.php
@@ -88,24 +88,24 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
         $cookieParams = array(
             'lifetime' => $cookie->getLifetime(),
             'path'     => $cookie->getPath(),
-            //'domain'   => $cookie->getConfigDomain(),
+           //  'domain'   => $cookie->getConfigDomain(),
            // 'secure'   => $cookie->isSecure(),
-            //'httponly' => $cookie->getHttponly()
+           //  'httponly' => $cookie->getHttponly()
         );
 
-        if (!$cookieParams['httponly']) {
-            unset($cookieParams['httponly']);
-            if (!$cookieParams['secure']) {
-                unset($cookieParams['secure']);
-                if (!$cookieParams['domain']) {
-                    unset($cookieParams['domain']);
-                }
-            }
-        }
+        // if (!$cookieParams['httponly']) {
+        //     unset($cookieParams['httponly']);
+        //     if (!$cookieParams['secure']) {
+        //         unset($cookieParams['secure']);
+        //         if (!$cookieParams['domain']) {
+        //             unset($cookieParams['domain']);
+        //         }
+        //     }
+        // }
 
-        if (isset($cookieParams['domain'])) {
-            $cookieParams['domain'] = $cookie->getDomain();
-        }
+        // if (isset($cookieParams['domain'])) {
+        //     $cookieParams['domain'] = $cookie->getDomain();
+        // }
 
         call_user_func_array('session_set_cookie_params', $cookieParams);
 
